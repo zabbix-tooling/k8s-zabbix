f355bb054178ce3db662cb64970dbae83f52ccc6
Marc Schöchlin <marc.schoechlin@flipapp.de>
2022-01-02 18:02:49 +0100

    change key

--- k8sobjects/daemonset.py
+++ k8sobjects/daemonset.py
@@ -7,18 +7,6 @@ from .k8sobject import K8sObject, transform_value
 logger = logging.getLogger(__file__)
 
 
-# same as statefulset
-# 'status': { 'collision_count': None,
-#             'conditions': None,
-#             'current_number_scheduled': 8,
-#             'desired_number_scheduled': 8,
-#             'number_available': 8,
-#             'number_misscheduled': 0,
-#             'number_ready': 8,
-#             'number_unavailable': None,
-#             'observed_generation': 8,
-#             'updated_number_scheduled': 8}}
-
 class Daemonset(K8sObject):
     object_type = 'daemonset'
 
@@ -57,13 +45,13 @@ class Daemonset(K8sObject):
 
             data_to_send.append(ZabbixMetric(
                 self.zabbix_host,
-                'check_kubernetesd[get,statefulsets,%s,%s,%s]' % (self.name_space, self.name, status_type),
+                'check_kubernetesd[get,daemonsets,%s,%s,%s]' % (self.name_space, self.name, status_type),
                 transform_value(self.resource_data[status_type]))
             )
 
         data_to_send.append(ZabbixMetric(
             self.zabbix_host,
-            'check_kubernetesd[get,statefulsets,%s,%s,available_status]' % (self.name_space, self.name),
+            'check_kubernetesd[get,daemonsets,%s,%s,available_status]' % (self.name_space, self.name),
             self.resource_data['available_status']))
 
         return data_to_send
